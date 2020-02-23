1d989bdc4820c7e7d0cb2363eb47332e4a9ee5a1
Brett Chaldecott <brett.chaldecott@absa.co.za>
2020-02-23 17:06:00 +0200

    1) update of libraries

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 export { default as AvertemKey } from './lib/key';
 export { default as AvertemStore } from './lib/store';
 export { default as AvertemTransaction } from './lib/transaction';
+export { default as AvertemModel } from './lib/model';
